74deefa58c8fed10ed74765e75749ad2f8c0ded6
Denedo Oghenetega <tegadev01@gmail.com>
2023-08-12 21:29:51 +0100

    Refactor mailgun client usage

--- helpers/index.js
+++ helpers/index.js
@@ -1,12 +1,4 @@
-import formData from "form-data";
-import Mailgun from "mailgun.js";
-
-const mailgun = new Mailgun(formData);
-
-export const mg = mailgun.client({
-  username: "api",
-  key: process.env.MAILGUN_API_KEY,
-});
+import { mg } from "./mailgun";
 
 export async function addToSubscribersList(email, confirmationCode = "") {
   try {
---
+++ helpers/mailgun.js
@@ -0,0 +1,9 @@
+import formData from "form-data";
+import Mailgun from "mailgun.js";
+
+const mailgun = new Mailgun(formData);
+
+export const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY,
+});
